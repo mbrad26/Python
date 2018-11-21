d305b20a00d24fc3f90991d41d1b0c8279a78524
Marius Brad <mbrad26@gmail.com>
2018-11-21 11:33:45 +0000

    ...

--- decorators.py
+++ decorators.py
@@ -1,4 +1,6 @@
+import functools
 from datetime import datetime
+import time
 
 
 def my_decorator(func):
@@ -24,7 +26,7 @@ def not_during_the_night(func):
         if 7 <= datetime.now().hour < 22:
             func()
         else:
-            pass  # Hush, the neighbors are asleep
+            pass
     return wrapper
 
 
@@ -35,4 +37,109 @@ def say_whee():
 say_whee = not_during_the_night(say_whee)
 
 print(say_whee())
+
+
+def do_twice(func):
+    def wrapper_do_twice():
+        func()
+        func()
+    return wrapper_do_twice
+
+
+@do_twice
+def say_whee():
+    print('Whee!')
+
+
+print(say_whee())
+
+
+def do_twice(func):
+    @functools.wraps(func)
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper_do_twice()
+
+
+# Timing Functions
+
+def timer(func):
+
+    @functools.wraps(func)
+    def wraper(*args, **kwargs):
+        start = time.perf_counter()
+        value = func(*args, **kwargs)
+        end = time.perf_counter()
+        run_time = end - start
+        print(f'Finished {func.__name__} in {run_time:.4f} secs.')
+        return value
+
+    return wraper
+
+
+@timer
+def sum_sqr(num):
+    for i in range(num):
+        sum([x**2 for x in range(10000)])
+
+
+print(sum_sqr(999))
+
+
+# Debugging code
+
+def debugging(func):
+
+    @functools.wraps(func)
+    def wraper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f'{k}={v!r}' for k, v in kwargs.items()]
+        signature = ', '.join(args_repr + kwargs_repr)
+        print(f'Calling {func.__name__}({signature})...')
+        value = func(*args, **kwargs)
+        print(f'{func.__name__!r} returned {value!r}')
+        return value
+    return wraper
+
+
+@debugging
+def greeting(name, age=None):
+    if age is None:
+        return f'Howdy {name}!'
+    else:
+        return f'Howdy {name}! {age} already, you are growing up!'
+
+
+greeting('Bob')
+greeting('Taylor', 18)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
