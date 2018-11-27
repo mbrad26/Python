5cedcde20dc4efa8d2670d3df3812507f14cdc86
Marius Brad <mbrad26@gmail.com>
2018-11-27 12:14:51 +0000

    ...

--- fp_1.py
+++ fp_1.py
@@ -35,7 +35,10 @@ class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-        
+
+    def __bool__(self):
+        return bool(abs(self))
+
     def __abs__(self):
         return hypot(self.x, self.y)
 
