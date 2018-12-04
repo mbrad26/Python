c9124e4ecf75de1d9d4db24760ad1444db4f5f6c
Marius Brad <mbrad26@gmail.com>
2018-12-04 18:03:11 +0000

    ..

---
+++ fp_7.py
@@ -0,0 +1,39 @@
+class Average:
+
+    def __init__(self):
+        self.series = []
+
+    def __call__(self, new_value):
+        self.series.append(new_value)
+        total = sum(self.series)
+        return total / len(self.series)
+
+
+avg = Average()
+
+print(avg(4))
+print(avg(5))
+print(avg(9))
+
+###############################################################
+
+
+def make_average():
+    series = []
+
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total / len(series)
+
+    return averager
+
+
+favg = make_average()
+
+print(favg(4))
+print(favg(5))
+print(favg(9))
+
+#######################################################################
+
