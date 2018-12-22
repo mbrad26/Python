348f77a34da68b4eb3d392d4780d8116e1b469ee
Marius Brad <mbrad26@gmail.com>
2018-12-22 10:00:43 +0000

    ..

--- fp_15.py
+++ fp_15.py
@@ -1,5 +1,6 @@
 import re
 import reprlib
+import operator
 from contextlib import contextmanager
 from functools import lru_cache
 from collections import defaultdict
@@ -93,7 +94,18 @@ for k, v in s:
 print(d)
 
 
+###############################################################
 
+
+metro_data = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+    ]
+
+print(sorted(metro_data, key=operator.itemgetter(1)))
 
 
 
